b86ffe1ca42b41c8499a5905167d494100757d69
milutinvelisic <ekonomska98@gmail.com>
2021-01-26 20:26:35 +0100

    added product page and product images

--- app/views/pages/product.php
+++ app/views/pages/product.php
@@ -1,1 +1,116 @@
-<h1>Product strana</h1>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Product strana</h1>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-4 mt-5 mb-5">
+            <div class="card">
+                <img class="card-img-top" src="public/img/orange-1.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Orange</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mt-5 mb-5">
+            <div class="card">
+                <img class="card-img-top" src="public/img/pink-grapefruit-1.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Pink Grapefruit</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mt-5 mb-5">
+            <div class="card">
+                <img class="card-img-top" src="public/img/white-grapefruit-1.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">White Grapefruit</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mt-5 mb-5">
+            <div class="card">
+                <img class="card-img-top" src="public/img/tangerine-1.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Tangerine</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mt-5 mb-5">
+            <div class="card">
+                <img class="card-img-top" src="public/img/lime-1.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Lime</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mt-5 mb-5">
+            <div class="card">
+                <img class="card-img-top" src="public/img/pomelo-1.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Pomelo</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mt-5 mb-5">
+            <div class="card">
+                <img class="card-img-top" src="public/img/kumquat-1.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Kumquat</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mt-5 mb-5">
+            <div class="card">
+                <img class="card-img-top" src="public/img/lemon-1.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Lemon</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mt-5 mb-5">
+            <div class="card">
+                <img class="card-img-top" src="public/img/mandarine-1.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Mandarine</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <div class="col-8">
+                <div class="card card-white post">
+                    <div class="post-heading">
+                        <div class="float-left image">
+<!--                            <img src="http://bootdey.com/img/Content/user_1.jpg" class="img-circle avatar" alt="user profile image">-->
+                        </div>
+                        <div class="float-left meta">
+                            <div class="title h5">
+                                <a href="#"><b>Ryan Haywood</b></a>
+                                made a post.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="post-description">
+                        <p>Bootdey is a gallery of free snippets resources templates and utilities for bootstrap css hmtl js framework. Codes for developers and web designers</p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    </div>
+</div>
