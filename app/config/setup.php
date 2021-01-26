a6aece8d5ec3e0c313036802621c0467ffab43c6
milutinvelisic <ekonomska98@gmail.com>
2021-01-26 11:23:40 +0100

    added configuration files

---
+++ app/config/setup.php
@@ -0,0 +1,25 @@
+<?php
+
+session_start();
+ob_start();
+
+define("ABSOLUTE_PATH", $_SERVER["DOCUMENT_ROOT"]);
+define("LOG", ABSOLUTE_PATH."/app/config/log.txt");
+
+
+function autoload($className)
+{
+    $className = ltrim($className, '\\');
+    $fileName  = '';
+    $namespace = '';
+    if ($lastNsPos = strrpos($className, '\\')) {
+        $namespace = substr($className, 0, $lastNsPos);
+        $className = substr($className, $lastNsPos + 1);
+        $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+    }
+    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+
+    require $fileName;
+}
+
+spl_autoload_register('autoload');
